a63705f18eb37062540a091888457827b86408b0
Chriss Chris <zenskech@outlook.com>
2021-01-05 18:41:18 +0500

    edit

--- review-page-react/src/App.js
+++ review-page-react/src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Review from "./Review";
-
 function App() {
   return (
     <main>
-      <section className="continer">
+      <section className="container">
         <div className="title">
-          <h2>Our reviews</h2>
+          <h2>our reviews</h2>
           <div className="underline"></div>
         </div>
+        <Review />
       </section>
-      <Review />
     </main>
   );
 }
